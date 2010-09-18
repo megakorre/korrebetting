f240a809b9faab8f0d6a00d44feb82ec08c60475
patrik_karlin <patrik.karlin@gmail.com>
2010-09-18 15:40:07 +0200

    added progress bar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+
+
 function JobbContext(args) {
   var jobs = args.jobs;
+  var count = jobs.length;
+  var items_prossesed = 0;
   
   function prossesJobb(job) {
     args.job(job, function() {
+      items_prossesed += 1;
+      args.progress(
+        (items_prossesed / count) * 100);
+      
       var job = jobs.pop();
       if(job !== undefined) {
         prossesJobb(job);
@@ -26,11 +34,23 @@ var data = null;
 
 $(function() {
   
+  $("#progressbar").progressbar({
+  	value: 0,
+  	animated: true
+  });
+  $("input:button").button();
+  
+  
   $("#download-games").click(function() {
+    $(this).button( "disable" );
+    $("#progressbar").slideDown("slow");
     $.getJSON('/games.json', {}, function(json, textStatus) {
       data = json;
       JobbContext({
         jobs: data.slice(),
+        progress: function(p) {
+          $("#progressbar").progressbar("value", p);
+        },
         job: function(item, callback) {
           $.getJSON('/games/statistics.json', {match: item.matchid }, function(json, textStatus) {
             for(i in data) {
